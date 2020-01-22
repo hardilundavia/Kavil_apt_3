b3c6da73c282fbba543fe57523644cce7b459c7e
hardilundavia <50069843+hardilundavia@users.noreply.github.com>
2020-01-22 10:04:39 +0530

    Add files via upload

---
+++ practical-3/p1/p1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer();
+            c.setDetails();
+            c.getInfo();
+            Console.ReadKey();
+        }
+    }
+    abstract class Car
+    {
+        public int carno;
+        public String name;
+        public abstract void getCarInfo();
+    }
+    interface Rate
+    {
+        void getDays();
+        void getKm();
+    }
+    class Customer:Car,Rate
+    {
+        String nm,s,d;
+        int days;
+        double km;
+        double total=0;
+        double rate_km = 7.5;
+        double rate_days = 500;
+        public void setDetails()
+        {
+            Console.WriteLine("Enter Name : ");
+            nm = Console.ReadLine();
+            Console.WriteLine("Enter Source Station Name : ");
+            s = Console.ReadLine();
+            Console.WriteLine("Enter Destination Station Name : ");
+            d = Console.ReadLine();
+            Console.WriteLine("Enter Car Name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Car No : ");
+            carno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter No of days : ");
+            days = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter No of Km : ");
+            km = Convert.ToDouble(Console.ReadLine());
+        }
+        public void getDays()
+        {
+            Console.WriteLine("Total Days :"+days);
+        }
+        public void getKm()
+        {
+            Console.WriteLine("Total Km : "+km);
+        }
+        public void calRate()
+        {
+            total = km * rate_km;
+            total += days * rate_days;
+        }
+        public void getInfo()
+        {
+            calRate();
+            Console.WriteLine();
+            Console.WriteLine("Customer Name : " + nm);
+            Console.WriteLine("Source Station Name : " + s);
+            Console.WriteLine("Destination Station Name : " + d);
+            getCarInfo();
+            getDays();
+            getKm();
+            Console.WriteLine("Total Amount :"+total);
+        }
+        public override void getCarInfo()
+        {
+            Console.WriteLine("Car Name : "+name);
+            Console.WriteLine("Car No :"+carno);
+        }
+    }
+}
---
+++ practical-3/p3_stack/p3_stack/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3_stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Example p1 = new Example();
+            p1.getStack();
+            
+        }
+    }
+    class Example
+    {    
+        public void getStack()
+        {
+            Stack<int> s1 = new Stack<int>();
+            int ch,pop,peek;
+            do
+            {
+                Console.WriteLine("1.push");
+                Console.WriteLine("2.Pop");
+                Console.WriteLine("3.Peep");
+                Console.WriteLine("4.Display");
+                Console.WriteLine("5.Count Number");
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("Enter Your Choice :");
+                ch = Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1: Console.WriteLine("Enter Number :");
+                            s1.Push(Convert.ToInt32(Console.ReadLine()));
+                            break;
+                    case 2: pop = s1.Pop();
+                            Console.WriteLine("Popped Elements : "+pop);
+                            break;
+                    case 3: peek=s1.Peek();
+                            Console.WriteLine("Peeked Elements : "+peek);
+                            break;
+                    case 4: foreach (var elem in s1)
+                            {
+                                Console.WriteLine(elem);
+                            }
+                            break;
+                    case 5: Console.WriteLine(s1.Count());
+                            break;
+                    default: Console.WriteLine("Invalid Number");
+                        break;
+                }
+            } while (ch != 0);
+
+        }
+    }
+}
